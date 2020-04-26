72650c9328101ea3954d3bc3e6edd2706811265b
dististik <austin4444@gmail.com>
2020-04-25 23:38:10 -0700

    Update random_emoji.js collectors

--- src/commands/scripts/random_emoji.js
+++ src/commands/scripts/random_emoji.js
@@ -3,13 +3,15 @@ const DiscordIDs = require("../../json/discord_ids.json");
 
 // Function for picking random property from object
 function randomProperty(obj){
+	// Create array of emoji keys
 	let keys = Array.from(obj.keys());
-	return obj.get(keys[Math.floor(Math.random() * keys.length)])
+	// Pick a random key to access the cache of guild emoji
+	return obj.cache.get(keys[Math.floor(Math.random() * keys.length)])
 };
 // Get a random emoji from Nameless' designated emoji guild
 exports.randomEmoji = function(client){
 	// Create variable for emoji guild
-	let emojiGuild = client.guilds.get(DiscordIDs.guilds.emoji);
+	let emojiGuild = client.guilds.cache.get(DiscordIDs.guilds.emoji);
 	// If the emoji guild is online, return a random emoji from it
 	if(emojiGuild.available)
 		return randomProperty(emojiGuild.emojis);
