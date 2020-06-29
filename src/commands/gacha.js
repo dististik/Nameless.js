064d00c5302180c3a932cc5c6143691f5532b2c3
dististik <austin4444@gmail.com>
2020-06-29 00:33:31 -0700

    Add functions for randomly selecting from Pokedex and add generic functionality to gacha command

--- src/commands/gacha.js
+++ src/commands/gacha.js
@@ -21,6 +21,45 @@ let userChoices = []; // Choices the current user may pick from
 let gachaMasterKey = []; // Array for clearing memory after successful round
 let gachaString = ""; // String for sending results of a single gacha roll
 
+// Function for returning array of 1 - 6 random pokemon based on provided criteria
+function gachaArray(format,number){
+	// Initialise array being returned
+	let returnArray = [];
+	// Initialise loop control variable
+	let i = 0;
+	// Randomly select the specified number from the array of possible rolls
+	while(i < number){
+		// Pick a random number to access the array of possible rolls with
+		let x = Math.floor(Math.random() * randpoke.length);
+		// Check if the requested number isn't already in the return array and legal in specified format
+		if(!returnArray.includes(randpoke[x]) && formatLegal(format,Pokedex[randpoke[x]])){
+			// If the return array does not have our randomly selected Pokemon, add it
+			returnArray.push(randpoke[x]);
+			// On successful add, increment control variable and restart loop 
+			i++; continue;
+		} else continue; // else, restart loop
+	}
+	// Return the return array
+	return returnArray;
+}
+// Function for checking format legality of randomly selected Pokemon
+function formatLegal(format,obj){
+	// Find specified format in switch statement
+	switch(format){
+		// Any Pokemon allowed
+		case "any":
+			return true;
+		// Checking for legality in current Smogon CAP singles format
+		case "cap":
+			if(!obj.uber) return true;
+			else return false;
+		// Checking for legality in current Nisemon Singles format
+		case "nisemon":
+			if(!obj.exiled) return true;
+			else return false;
+	}
+}
+
 module.exports = {
 	name: "gacha",
 	description: "Facilitates roulette draws and rolls.",
@@ -32,19 +71,23 @@ module.exports = {
 		if(args[0] == 'any' || args[0] == 'cap' || args[0] == 'nisemon'){
 			// Check if a valid number was provided alongside the format
 			if(!(+args[1]) || args[1] > 6 || args[1] < 1) {
-				message.channel.send("Please provide a valid number of draws to perform (1-6)");
+				message.channel.send("Please provide a valid number of draws to perform. (1-6)");
 				return;
 			}
-			// Initialise a while loop variable
-			let i = 0;
-			// Run a draw depending on the format provided
-			switch(args[0]){
-				case "any" : // Draw to run using the full list of included Pokemon
-					while(i < agrs[1]){
-						
-					}
+			// Initialise array of randomly selected Pokemon legal in specified format
+			let gachaRoll = gachaArray(args[0],args[1]).slice();
+			// Constructing generic response
+			// Initialise response string
+			let response = "You draw:";
+			// Loop through recieved roll and copy contents into response
+			for(i = 0; i < gachaRoll.length; i++){
+				response += `\n[${i+1}] **${gachaRoll[i]}**`;
 			}
+			message.channel.send(response);
 		}
-
+		else {
+			message.channel.send("Please request a valid format.");
+			return;
+		}
 	}
 }
